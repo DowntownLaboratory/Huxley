3fa8f878e9c0bf08dff14950b55914ad593d007b
ralcaraz <ralcarazerhs@gmail.com>
2021-01-01 02:58:17 -0800

    Added initialization file

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,7 @@
 mkdir -p /docker
 chown docker:docker /docker
+groupadd -g 5050 pgadmin
+useradd -r -u 5050 -g 5050 -s /usr/bin/nologin pgadmin
+mkdir -p /docker/pgadmin
+chown pgadmin:pgadmin /docker/pgadmin
 #possible improvement is to automate creation of .env file with user id and group id
